335c3ac4c59c819f1308d4805d59aa24e468fa15
MatiasOso <114182943+MatiasOso@users.noreply.github.com>
2023-07-20 02:56:34 +0000

    MatiasOsores

--- mycommerce/components/Cart/index.jsx
+++ mycommerce/components/Cart/index.jsx
@@ -128,12 +128,12 @@ export default function Cart() {
                         <p>$82.00</p>
                       </div>
                       <p className="mt-0.5 text-sm text-gray-500 bg-crema">Envío e impuestos calculados al finalizar la compra.</p>
-                      <div className="mt-6">
+                      <div className="mt-6" >
                         <a
                           href="#"
-                          className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+                          className="flex items-center justify-center rounded-md border border-transparent bg-morado px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
                         >
-                          Checkout
+                          Finalizar compra
                         </a>
                       </div>
                       <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
